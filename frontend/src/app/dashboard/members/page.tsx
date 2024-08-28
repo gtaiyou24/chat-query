d54ea5a1d206180773eb58a328c4682b796bc44a
gtaiyou24 <gtaiyou24@gmail.com>
2024-08-28 11:52:54 +0900

    feat: invite-button

--- frontend/src/app/dashboard/members/page.tsx
+++ frontend/src/app/dashboard/members/page.tsx
@@ -3,6 +3,7 @@ import {columns} from "@/components/member/columns";
 import {getMembers} from "@/lib/api";
 import {auth} from "@/lib/auth";
 import {notFound} from "next/navigation";
+import InviteButton from "@/components/member/invite-button";
 
 
 export default async function MembersPage() {
@@ -10,7 +11,11 @@ export default async function MembersPage() {
     if (!session) notFound();
     const members = await getMembers(session.currentProject?.tenantId ?? session.user.tenants[0].id);
     return (
-        <div className="container mx-auto mt-8">
+        <div className="container mx-auto px-48">
+            <div className="flex justify-between gap-4 my-8">
+                <h1 className="text-2xl font-bold">チームメンバー</h1>
+                <InviteButton />
+            </div>
             <MemberTable columns={columns} data={members} />
         </div>
     );
